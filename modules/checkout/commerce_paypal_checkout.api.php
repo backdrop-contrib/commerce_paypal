cedcda9795b3cca728274887164b6dd071e0d1e3
Jonathan Sacksick <jonathan.sacksick@gmail.com>
2019-03-25 12:45:18 +0100

    Issue #3043112 by jsacksick: Add a hook_commerce_paypal_checkout_update_order_request_alter().

--- modules/checkout/commerce_paypal_checkout.api.php
+++ modules/checkout/commerce_paypal_checkout.api.php
@@ -17,3 +17,16 @@
 function hook_commerce_paypal_checkout_create_order_request_alter(&$request_body, $order) {
  // No example.
 }
+
+/**
+ * Allows modules to alter the request body before the update order API call is
+ * made to PayPal.
+ *
+ * @param array $request_body
+ *   The request body.
+ * @param $order
+ *   The order.
+ */
+function hook_commerce_paypal_checkout_update_order_request_alter(&$request_body, $order) {
+  // No example.
+}
